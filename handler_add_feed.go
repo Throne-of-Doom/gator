94a050da0de2f12e3c2fe6efcba3195a7801298a
Throne-of-Doom <cg-86@live.com>
2025-11-11 18:37:58 -0500

    added feeds SQL, and addFeed command

---
+++ handler_add_feed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Throne-of-Doom/gator/internal/database"
+)
+
+func handlerAddFeed(s *state, cmd command) error {
+	ctx := context.Background()
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+	if len(cmd.args) != 2 {
+		return fmt.Errorf("usage: addfeed <name> <url>")
+	}
+	name := cmd.args[0]
+	url := cmd.args[1]
+
+	feed, err := s.db.CreateFeed(ctx, database.CreateFeedParams{
+		Name:   name,
+		Url:    url,
+		UserID: user.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't create feed: %w", err)
+	}
+	printFeed(feed)
+	return nil
+}
+
+// go
+func printFeed(feed database.Feed) {
+	fmt.Printf("* ID:      %s\n", feed.ID)
+	fmt.Printf("* Created: %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated: %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:    %s\n", feed.Name)
+	fmt.Printf("* URL:     %s\n", feed.Url)
+	fmt.Printf("* UserID:  %s\n", feed.UserID)
+}
---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE feeds (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    name TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    user_id uuid NOT NULL,
+    CONSTRAINT fk_user_id
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE feeds;
